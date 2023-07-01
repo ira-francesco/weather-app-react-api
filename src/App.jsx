089abd9406d2affa5dd8845313f1f51a62d030e0
Papierkorb <62933268+PapierKorb76@users.noreply.github.com>
2023-07-01 22:04:57 +0200

    Start to Styling the GUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import { useState } from "react";
+import "./style.scss";
+import backgroundImage from "./assets/background.jpg";
 export default function App() {
   const apiKey = "d54919d21826957b16fa1de8e3099d25";
   const apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-  const [weatherObj, setWeatherObj] = useState({ name: "" });
+  const [weatherObj, setWeatherObj] = useState({
+    name: "",
+    weatherMain: "",
+    temp: "",
+    feelsLike: "",
+    humidity: "",
+    country: "",
+    main: "",
+    icon: ""
+  });
+
   const [location, setLocation] = useState("London");
   async function checkWeather() {
     const response = await fetch(apiUrl + location + `&appid=${apiKey}`);
     var data = await response.json();
     console.log(data);
-    setWeatherObj({ name: data.name });
+    setWeatherObj({
+      name: data.name,
+      weatherMain: data.weather[0].main,
+      temp: `${Math.round(Number(data.main.temp))}° C`,
+      feelsLike: `${Math.round(Number(data.main.feels_like))}° C`,
+      humidity: `${data.main.humidity}%`
+    });
   }
 
   return (
-    <div className="App">
+    <div className="App" style={{ background: `url(${backgroundImage})` }}>
       <input
         type="text"
         onChange={(e) => {
@@ -23,6 +41,10 @@ export default function App() {
       />
       <button onClick={checkWeather}>Check the weather!</button>
       <h1>{weatherObj.name}</h1>
+      <h1>{weatherObj.weatherMain}</h1>
+      <h1>{weatherObj.temp}</h1>
+      <h1>{weatherObj.feelsLike}</h1>
+      <h1>{weatherObj.humidity}</h1>
     </div>
   );
 }
