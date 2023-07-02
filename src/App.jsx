3a3f5a35844c471f713d8f6b98a5dcc8b5123bf7
Papierkorb <62933268+PapierKorb76@users.noreply.github.com>
2023-07-02 17:46:12 +0200

    Implementing and Styling Location Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,20 @@ export default function App() {
   const apiUrl =
     "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+  const iconUrl = "http://openweathermap.org/img/w/";
+  /*
+  + iconcode + ".png";
+  */
   const [weatherObj, setWeatherObj] = useState({
     name: "",
+    country: "",
     weatherMain: "",
     temp: "",
     feelsLike: "",
     humidity: "",
-    country: "",
     main: "",
-    icon: ""
+    icon: "",
+    windSpeed: ""
   });
 
   const [location, setLocation] = useState("London");
@@ -23,28 +28,42 @@ export default function App() {
     var data = await response.json();
     console.log(data);
     setWeatherObj({
-      name: data.name,
+      name: `${data.name}, ${data.sys.country}`,
       weatherMain: data.weather[0].main,
       temp: `${Math.round(Number(data.main.temp))}° C`,
       feelsLike: `${Math.round(Number(data.main.feels_like))}° C`,
-      humidity: `${data.main.humidity}%`
+      humidity: `${data.main.humidity}%`,
+      icon: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
     });
   }
 
   return (
     <div className="App" style={{ background: `url(${backgroundImage})` }}>
-      <input
-        type="text"
-        onChange={(e) => {
-          setLocation(e.target.value);
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center"
         }}
-      />
-      <button onClick={checkWeather}>Check the weather!</button>
-      <h1>{weatherObj.name}</h1>
-      <h1>{weatherObj.weatherMain}</h1>
-      <h1>{weatherObj.temp}</h1>
-      <h1>{weatherObj.feelsLike}</h1>
-      <h1>{weatherObj.humidity}</h1>
+      >
+        <input
+          type="text"
+          onChange={(e) => {
+            setLocation(e.target.value);
+          }}
+          style={{ marginTop: "20px" }}
+        />
+        <button style={{ marginTop: "20px" }} onClick={checkWeather}>
+          Check the weather!
+        </button>
+      </div>
+      <div className={"main-container"}>
+        <h1>{weatherObj.name}</h1>
+        <h1>{weatherObj.weatherMain}</h1>
+        <h1>{weatherObj.temp}</h1>
+        <h1>{weatherObj.feelsLike}</h1>
+        <h1>{weatherObj.humidity}</h1>
+        <img src={weatherObj.icon} alt={"weather-icon"} />
+      </div>
     </div>
   );
 }
